31f026dc5ab0ded2746b2cd689a3a7a2aa8116a0
Thanh Hosy <hosythanh14@gmail.com>
2018-10-06 16:00:33 +0700

    Created contact-us.php

---
+++ FrontEnd/contact-us.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<?php include 'header.php' ?>
+
+<div class="contact-content">
+
+	<h1 class="position-relative contact-title">LIÊN HỆ VỚI CHÚNG TÔI</h1>
+	<div class="position-relative">
+		<div class="contact-company-info">
+			Email: tourzy@gmail.com
+			<br>
+			Số điện thoại: 0919 111 444
+			<br>
+			Địa chỉ: 123 Thái Hà, Đống Đa, Hà Nội
+		</div>
+		<div class="contact-box">
+			<p id="contact-msg">Liên hệ với chúng tôi để nhận tư vấn dịch vụ và hỗ trợ cho những vấn đề của bạn/ doanh nghiệp </p><br>
+
+			<div class="contact-input-info">
+				<p class="contact-input-field-name">Họ tên * </p>
+				<textarea name="Text1" cols="37" rows="1" style="resize: none;"></textarea>
+				<p class="contact-input-field-name">Địa chỉ email * </p>
+				<textarea name="Text1" cols="37" rows="1" style="resize: none;"></textarea>
+				<p class="contact-input-field-name">Số điện thoại * </p>
+				<textarea name="Text1" cols="37" rows="1" style="resize: none;"></textarea>
+				<p class="contact-input-field-name">Lời nhắn * </p>
+				<textarea name="Text1" cols="37" rows="2" style="resize: none;"></textarea>
+				<a class="contact-submit-button" href="#">Gửi đến Tourzy</a>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+
+<?php include 'footer.php' ?>

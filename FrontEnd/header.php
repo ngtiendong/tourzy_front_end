880441f37d3b4e5e0c55768d9969a60aef3883fa
Thanh Hosy <hosythanh14@gmail.com>
2018-10-06 14:54:46 +0700

    Created header.php

---
+++ FrontEnd/header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Contains header content
+ *
+ * @author: Thanh Hosy
+ */
+?>
+
+
+<div class="header">
+	<div class="header-col">
+		<a class="tourzy-button" href="#">Tourzy</a>
+	</div>
+	<div class="header-col menu-item"><a href="#">Trang chủ</a></div>
+	<div class="header-col menu-item"><a href="#">Dịch vụ</a></div>
+	<div class="header-col menu-item"><a href="#">Thư viện</a></div>
+	<div class="header-col menu-item"><a href="#">Tin tức</a></div>
+	<div class="header-col menu-item"><a href="#">Liên hệ</a></div>
+	<div class="searchbox">
+		<input type='text' placeholder='Tìm kiếm dịch vụ' id='search-text-input' />
+		<div id='button-holder'>
+			<img src='img/magnifying_glass.png' />
+		</div>
+	</div>
+</div>

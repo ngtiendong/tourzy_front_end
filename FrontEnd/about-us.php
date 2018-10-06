0ad3313196c91fcb878ceb2ed521044e356a8097
Thanh Hosy <hosythanh14@gmail.com>
2018-10-06 13:43:07 +0700

    Created footer.php

---
+++ FrontEnd/about-us.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<?php include 'footer.php' ?>
---
+++ FrontEnd/footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content
+ *
+ * @author: Thanh Hosy
+ */
+?>
+
+<div class="footer">
+
+	<div class="column" id="tourzy">
+		<a class="tourzy-button" href="#">Tourzy</a>
+	</div>
+
+	<div class="column" id="thongtinchung">
+		<table>
+			<tr><td><h2>Thông tin chung</h2></td></tr>
+			<tr><td><a class="normal-link" href="#">Về chúng tôi</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Điều khoản yêu cầu</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Liên hệ</a></td></tr>
+		</table>
+	</div>
+
+	<div class="column" id="danhmuc">
+		<table>
+			<tr><td><h2>Danh mục</h2></td></tr>
+			<tr><td><a class="normal-link" href="#">Ảnh 360</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Video 360</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Video trải nghiệm</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Thư viện</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Dịch vụ</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Blog</a></td></tr>
+		</table>
+	</div>
+
+	<div class="column" id="congdong">
+		<table>
+			<tr><td><h2>Cộng đồng</h2></td></tr>
+			<tr><td><a class="normal-link" href="#">Youtube</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Facebook</a></td></tr>
+			<tr><td><a class="normal-link" href="#">Instagram</a></td></tr>
+		</table>
+	</div>
+
+</div>
